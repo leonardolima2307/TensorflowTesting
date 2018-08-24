1565de20ec67c2f5e0674ffc06b21152b794638b
leonardo <lima.l@gameonmedia.ie>
2018-08-24 16:47:53 +0100

    first commit

---
+++ tensorflow-without-a-phd-master/tensorflow-mnist-tutorial/mnist_1.0_softmax.py
@@ -0,0 +1,214 @@
+# encoding: UTF-8
+# Copyright 2016 Google.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import tensorflow as tf
+import tensorflowvisu
+import mnistdata
+import math
+print("Tensorflow version " + tf.__version__)
+tf.set_random_seed(0)
+
+# neural network with 1 layer of 10 softmax neurons
+#
+# · · · · · · · · · ·       (input data, flattened pixels)       X [batch, 784]        # 784 = 28 * 28
+# \x/x\x/x\x/x\x/x\x/    -- fully connected layer (softmax)      W [784, 10]     b[10]
+#   · · · · · · · ·                                              Y [batch, 10]
+
+# The model is:
+#
+# Y = softmax( X * W + b)
+#              X: matrix for 100 grayscale images of 28x28 pixels, flattened (there are 100 images in a mini-batch)
+#              W: weight matrix with 784 lines and 10 columns
+#              b: bias vector with 10 dimensions
+#              +: add with broadcasting: adds the vector to each line of the matrix (numpy)
+#              softmax(matrix) applies softmax on each line
+#              softmax(line) applies an exp to each value then divides by the norm of the resulting line
+#              Y: output matrix with 100 lines and 10 columns
+
+# Download images and labels into mnist.test (10K images+labels) and mnist.train (60K images+labels)
+mnist = mnistdata.read_data_sets("data", one_hot=True, reshape=False)
+
+# input X: 28x28 grayscale images, the first dimension (None) will index the images in the mini-batch
+X = tf.placeholder(tf.float32, [None, 28, 28, 1])
+# correct answers will go here
+Y_ = tf.placeholder(tf.float32, [None, 10])
+# weights W[784, 10]   784=28*28
+W = tf.Variable(tf.zeros([784, 10]))
+# biases b[10]
+b = tf.Variable(tf.zeros([10]))
+
+W1 = tf.Variable(tf.truncated_normal([28*28, 200] ,stddev=0.1))
+B1 = tf.Variable(tf.zeros([200]))
+
+W2 = tf.Variable(tf.truncated_normal([200, 10], stddev=0.1))
+B2 = tf.Variable(tf.zeros([10]))
+
+step = tf.placeholder(tf.int32)
+
+# flatten the images into a single line of pixels
+# -1 in the shape definition means "the only possible dimension that will preserve the number of elements"
+XX = tf.reshape(X, [-1, 784])
+
+train_X, train_Y = mnist.train.next_batch(1000)
+
+#print('mnist is  ',mnist.__getattribute__)
+print('shape train_X is len is', train_X.shape, len(train_X))
+#print('shape Y_ is ', train_X[0] )
+
+# The model
+#Y = tf.nn.softmax(tf.matmul(XX, W) + b)
+
+##MAKING THE dropout
+pkeep = tf.placeholder(tf.float32)
+Y1 = tf.nn.relu(tf.matmul(XX, W1) + B1)
+Y1d = tf.nn.dropout(Y1, pkeep)
+
+Y = tf.nn.softmax(tf.matmul(Y1d, W2) + B2)
+
+#Y  = tf.nn.softmax(tf.matmul(Y1, W2) + B2)
+##
+##comented to remove the cross entrompy, failed to graph
+#Ylogits = tf.matmul(Y1, W2) + B2
+#Y = tf.nn.softmax(Ylogits)
+
+# loss function: cross-entropy = - sum( Y_i * log(Yi) )
+#                           Y: the computed output vector
+#                           Y_: the desired output vector
+
+# cross-entropy
+# log takes the log of each element, * multiplies the tensors element by element
+# reduce_mean will add all the components in the tensor
+# so here we end up with the total cross-entropy for all images in the batch
+##
+## Replacing
+cross_entropy = -tf.reduce_mean(Y_ * tf.log(Y)) * 1000.0  # normalized for batches of 100 images,
+                                                          # *10 because  "mean" included an unwanted division by 10
+#cross_entropy = tf.nn.softmax_cross_entropy_with_logits(logits=Ylogits, labels=Y_)
+# accuracy of the trained model, between 0 (worst) and 1 (best)
+correct_prediction = tf.equal(tf.argmax(Y, 1), tf.argmax(Y_, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+# training, learning rate = 0.005
+#train_step = tf.train.GradientDescentOptimizer(0.005).minimize(cross_entropy)
+
+#learning_rate = 0.003
+
+learning_rate = 0.0001 + tf.train.exponential_decay(0.003, step, 2000, 1/math.e)
+##Changing the optimazer is the reason if the imporvement, not the number of layers in the neuronal network
+train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
+# matplotlib visualisation
+
+allweights = tf.concat([tf.reshape(W1, [-1]), tf.reshape(W2, [-1])], 0)
+allbiases  = tf.concat([tf.reshape(B1, [-1]), tf.reshape(B2, [-1])], 0)
+I = tensorflowvisu.tf_format_mnist_images(X, Y, Y_)  # assembles 10x10 images by default
+It = tensorflowvisu.tf_format_mnist_images(X, Y, Y_, 1000, lines=25)  # 1000 images on 25 lines
+datavis = tensorflowvisu.MnistDataVis()
+
+# init
+init = tf.global_variables_initializer()
+sess = tf.Session()
+sess.run(init)
+
+
+# You can call this function in a loop to train the model, 100 images at a time
+def training_step(i, update_test_data, update_train_data):
+
+    # training on batches of 100 images with 100 labels
+    batch_X, batch_Y = mnist.train.next_batch(100)
+
+    # compute training values for visualisation
+    if update_train_data:
+        a, c, im, w, b, l = sess.run([accuracy, cross_entropy, I, allweights, allbiases, learning_rate], feed_dict={X: batch_X, Y_: batch_Y, step: i, pkeep: 0.75})
+        datavis.append_training_curves_data(i, a, c)
+        datavis.append_data_histograms(i, w, b)
+        datavis.update_image1(im)
+       #print(str(i) + ": accuracy:" + str(a) + " loss: " + str(c))
+
+    # compute test values for visualisation
+    if update_test_data:
+        a, c, im = sess.run([accuracy, cross_entropy, It], feed_dict={X: mnist.test.images, Y_: mnist.test.labels,  pkeep: 1})
+        datavis.append_test_curves_data(i, a, c)
+        datavis.update_image2(im)
+        #print(str(i) + ": ********* epoch " + str(i*100//mnist.train.images.shape[0]+1) + " ********* test accuracy:" + str(a) + " test loss: " + str(c))
+
+    # the backpropagation training step
+    sess.run(train_step, feed_dict={X: batch_X, Y_: batch_Y, step: i, pkeep: 0.75})
+
+###CHANGE THE NUMBER OF ITERATIONS, 100 ONLY FOR MAKE IT FASTER TO RUN
+datavis.animate(training_step, iterations=6000+1, train_data_update_freq=10, test_data_update_freq=50, more_tests_at_start=True)
+
+# to save the animation as a movie, add save_movie=True as an argument to datavis.animate
+# to disable the visualisation use the following line instead of the datavis.animate line
+# for i in range(2000+1): training_step(i, i % 50 == 0, i % 10 == 0)
+
+print("max test accuracy: " + str(datavis.get_max_test_accuracy()))
+
+# final max test accuracy = 0.9268 (10K iterations). Accuracy should peak above 0.92 in the first 2000 iterations.
+
+
+
+
+######
+##### Try to do the same using sklearn
+######
+from sklearn.naive_bayes import GaussianNB
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier,  AdaBoostClassifier
+
+features, labels = mnist.train.next_batch(10000)
+
+features = np.reshape(features, [-1, 784])
+
+print('features after reshape is len is', features.shape, len(features))
+
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.05, random_state = 30)
+
+#clf = AdaBoostClassifier(n_estimators = 100) 
+#clf = GaussianNB()
+clf = RandomForestClassifier(n_estimators = 10, criterion = 'entropy', random_state = 42)
+clf.fit(X_train, y_train)
+
+
+print('shape y_train is len is', y_train.shape, len(y_train))
+print('shape X_train is len is', X_train.shape, len(X_train))
+
+
+print('Start training')
+clf.fit(X_train, y_train)
+#print(clf.feature_importances_, 'number features', clf.n_features_)
+print('End training')
+score = clf.score(X_test, y_test)
+
+###SCORING 
+from sklearn.metrics import classification_report
+y_pred = clf.predict(X_test)
+target_names = ['class 0', 'class 1', 'class 2']
+print(classification_report(y_test, y_pred))
+
+from sklearn.metrics import cohen_kappa_score
+##Not supported
+#print('cohen_kappa_score', cohen_kappa_score(y_test, y_pred))
+
+from sklearn.metrics import hamming_loss
+print('hamming_loss', hamming_loss(y_test, y_pred))
+
+print('normal score is', score)
+## NOT SUPPPORTED 
+##from sklearn.metrics import confusion_matrix
+##cm = confusion_matrix(y_test, y_pred)
+##print(cm)
+##tn, fp, fn, tp = cm.ravel()
+##print('(tn, fp, fn, tp)', tn, fp, fn, tp)
